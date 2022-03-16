005a76accdf8adeee6c73b9b2c35e8d44c34f42b
Anton Berdnikov <as.berdnikov@iaaras.ru>
2022-03-16 15:41:59 +0300

    Add template list debag

--- main/tszh_helper.py
+++ main/tszh_helper.py
@@ -2,18 +2,24 @@ import pandas as pd
 from tkinter import filedialog
 
 
-file_name = filedialog.askopenfilename()
+def member_list_import():
+    # чтение файла
+    filename = filedialog.askopenfilename()
+    data = pd.read_excel(filename)
 
-#извлечение списка собственников из реестра
-data = pd.read_excel(filename)
-data = data.drop(data[data['Unnamed: 2'].isna()].index)
-data = data.iloc[2:].reset_index()
+    # извленчение нужных данных (имена, номера квартир, площадь)
+    data = data.drop(data[data['Unnamed: 2'].isna()].index)
+    data = data.iloc[2:].reset_index()
+    members = pd.DataFrame(
+        {'name': data['Unnamed: 3'] + ' ' + data['Unnamed: 4'] + ' ' + data['Unnamed: 5'],
+            'flat': data['Unnamed: 2'],
+            'square': data['Unnamed: 7']})
+    members = members.astype({'square': 'float'})
+    return members
 
-tenant_list = pd.DataFrame({'name': data['Unnamed: 3'] + ' ' + data['Unnamed: 4'] + ' ' + data['Unnamed: 5'],
-                      'flat' : data['Unnamed: 2'],
-                      'square': data['Unnamed: 7']})
-tenant_list = tenant_list.astype({'square': 'float' })
 
+
+'''
 # создание по шаблона
 temp_list = [1, 3, 4, 10, 11, '5 Н']
 template = '1010011'
@@ -22,4 +28,8 @@ result = pd.DataFrame({'flat':temp_list,
                      'присутствовал': 1})
 for i in range(len(temp)):
     column_name = 'Вопрос ' + str(i+1)
-    result[column_name] = int(temp[i])
+    result[column_name] = int(temp[i])'''
+
+if __name__ == '__main__':
+    member_list = member_list_import()
+    print(member_list.head(2).to_string(), '\n', member_list.tail(2).to_string())
